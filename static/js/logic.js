0d12ec9d22d2289361d8804016794001774e6f71
kristinusry <67609941+kristinusry@users.noreply.github.com>
2020-11-15 09:36:08 -0800

    Update logic with markers

--- static/js/logic.js
+++ static/js/logic.js
@@ -5,15 +5,60 @@ var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_we
 var myMap = L.map("mapid", {
     center: [37.7749, -122.4194],
     zoom: 4
-  });
+});
   
-  // Adding tile layer
-  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+// Adding tile layer
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
     maxZoom: 18,
     zoomOffset: -1,
     id: "mapbox/streets-v11",
     accessToken: API_KEY
-  }).addTo(myMap);
+}).addTo(myMap);
 
+
+// Load in the API JSON
+d3.json(queryUrl, function(data) {
+  
+    // Loop through each earthquake
+    for (var i = 0; i < data.features.length; i++) {
+ 
+        // Store the magnitude
+        earthquakeMag = data.features[i].properties.mag
+        earthquakeMag = +earthquakeMag
+  
+        // Store the coordinates
+        cords = data.features[i].geometry.coordinates
+        cords[0] = +cords[0];
+        cords[1] = +cords[1];
+  
+        // Store the depth
+        depth = data.features[i].geometry.coordinates
+        depth[2] = +depth[2];
+
+        // Radius of marker
+        markerRadius = markerSize(earthquakeMag)
+        markerRaidus = +markerRadius
+  
+        // Plot the marker
+        var circle = L.circle([cords[1], cords[0]], {
+            fillOpacity: 0.75,
+            radius: markerRadius,
+            color: "white",
+            fillColor: "white"
+        })
+  
+        // Pop up for markers
+        circle.bindPopup(
+            "<h2>" + data.features[i].properties.place + "</h2>" +
+            "<h3>" + "Magnitude: " + earthquakeMag + "</h3>" +
+            "<p>" + data.features[i].properties.time + "</p>"           
+        ).addTo(myMap);               
+    }
+  });
+
+// Function to determine size of marker (based on magnitude)
+function markerSize(earthquakeMag) {
+    return earthquakeMag * 100000;
+ }
